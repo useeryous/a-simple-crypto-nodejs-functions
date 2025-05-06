fd1d3f3c89b42d5418a2c59d63ac0a2b04a6662c
lemon <103284423+useeryous@users.noreply.github.com>
2025-05-06 21:09:59 +0000

    modified:   main/passkey.js

--- main/passkey.js
+++ main/passkey.js
@@ -1,2 +1,27 @@
-const crypto = require ('crypto');
+const crypto = require('crypto');
 
+function encrypt(plainText) {
+  const key = crypto.randomBytes(32);
+  const iv = crypto.randomBytes(16);
+  const algorithm = 'aes-256-cbc';
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
+  let encryptedData = cipher.update(plainText, 'utf8', 'hex');
+  encryptedData += cipher.final('hex');
+  return {
+    iv: iv.toString('hex'),
+    key: key.toString('hex'),
+    encryptedData: encryptedData
+  };
+}
+
+function decrypt(encryptedData, key, iv, algorithm = 'aes-256-cbc') {
+    const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key, 'hex'), Buffer.from(iv, 'hex'));
+    let decryptedData = decipher.update(encryptedData, 'hex', 'utf8');
+    decryptedData += decipher.final('utf8');
+    return decryptedData;
+}
+
+export default {
+    encrypt,
+    decrypt
+}
